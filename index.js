93aa737bb04b1fc55c6ea8f65cbbd8c93b7f680f
Jim Kang <jimkang@gmail.com>
2016-05-15 15:01:56 -0400

    Started.

---
+++ index.js
@@ -0,0 +1,24 @@
+var compact = require('lodash.compact');
+// \W will match apostrophes, unfortunately.
+var basicWordBoundaryRegex = /[ ",;!?]/;
+var wordBoundariesInURLsRegex = /[.\/:\-_#]/;
+var urlRegex = /http.?:\/\/[^\s]+/;
+
+function splitToWords(text) {
+  var chunks = compact(text.split(basicWordBoundaryRegex));
+  chunks = splitChunksThatAreNotURLsEvenFurther(chunks);
+  return chunks;
+}
+
+function splitChunksThatAreNotURLsEvenFurther(chunks) {
+  for (var i = chunks.length - 1; i > -1; --i) {
+    var chunk = chunks[i];
+    debugger;
+    if (chunk.indexOf('/') !== -1 && !chunk.match(urlRegex)) {
+      chunks.splice.apply(chunks, [i, 1].concat(chunk.split(wordBoundariesInURLsRegex)));
+    }
+  }
+  return chunks;
+}
+
+module.exports = splitToWords;
---
+++ tests/basictests.js
@@ -0,0 +1,26 @@
+var test = require('tape');
+var splitToWords = require('../index');
+
+var testCases = [
+  {
+    text: 'I bomb atomically/Socrates\' philosophies and hypotheses/Can\'t define how I be droppin\' these mockeries/Lyrically perform armed robbery/Flee with the lottery/Possibly, they spotted me!',
+    expected: [ 'I', 'bomb', 'atomically', 'Socrates\'', 'philosophies', 'and', 'hypotheses', 'Can\'t', 'define', 'how', 'I', 'be', 'droppin\'', 'these', 'mockeries', 'Lyrically', 'perform', 'armed', 'robbery', 'Flee', 'with', 'the', 'lottery', 'Possibly', 'they', 'spotted', 'me' ]
+  },
+  {
+    text: 'So @username\'s app uses @othername. Here is a url: http://yo.com/2345/#/single-page-app  Cool, amirite?',
+    expected: [ 'So', '@username\'s', 'app', 'uses', '@othername.', 'Here', 'is', 'a', 'url:', 'http://yo.com/2345/#/single-page-app', 'Cool', 'amirite' ]
+  },
+  {
+    text: 'If a sentence follows this sentence, it is not true. The previous sentence is true.',
+    expected: [ 'If', 'a', 'sentence', 'follows', 'this', 'sentence', 'it', 'is', 'not', 'true.', 'The', 'previous', 'sentence', 'is', 'true.' ]
+  }
+];
+
+testCases.forEach(runTest);
+
+function runTest(testCase) {
+  test(testCase.text, function basicTest(t) {
+    t.deepEqual(splitToWords(testCase.text), testCase.expected, 'Words split correctly.');
+    t.end();
+  });
+}
